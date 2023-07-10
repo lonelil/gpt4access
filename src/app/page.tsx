ced532bc22e5c835f1e0bc6881da1e39cb6e0229
lonelil <51315646+lonelil@users.noreply.github.com>
2023-07-10 17:12:53 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,136 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { AiOutlineLoading } from "react-icons/ai";
+import { FaCheck } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+
+type Input = {
+  apiKey: string;
+};
+
+function maskKey(key: string) {
+  let prefix = key.slice(0, 3);
+  let maskedKey = prefix + key.slice(-4).padStart(key.length - 3, "*");
+  return maskedKey;
+}
+
+function isValidKey(key: string) {
+  return key.startsWith("sk-") && key.length === 51 ? true : false;
+}
 
 export default function Home() {
+  const { register, handleSubmit, reset } = useForm<Input>();
+  const [page, setPage] = useState("home");
+  const [details, setDetails] = useState("");
+  const [key, setKey] = useState("");
+  const onSubmit: SubmitHandler<Input> = async (data) => {
+    reset();
+
+    if (!isValidKey(data.apiKey)) {
+      setDetails(`Invalid API Key.`);
+      setPage("bad");
+      return;
+    }
+
+    setPage(`loading`);
+    setKey(data.apiKey);
+    const res = await fetch("https://api.openai.com/v1/models", {
+      headers: {
+        Authorization: `Bearer ${data.apiKey}`,
+      },
+    });
+
+    if (!res.ok) {
+      setDetails(`Invalid API Key.`);
+      setPage("bad");
+
+      return;
+    }
+
+    const models = await res.json();
+
+    if (models.data.find((model: any) => model.id === "gpt-4")) {
+      setPage("good");
+      return;
+    }
+
+    setDetails(`Your key does not have GPT-4.`);
+    setPage("bad");
+  };
+
+  const returnHome = () => {
+    setKey("");
+    setDetails("");
+    setPage("home");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-screen bg-black text-white flex justify-center flex-col items-center">
+      {key ? <p className="text-zinc-500 mb-12">{maskKey(key)}</p> : <></>}
+      {page === "home" ? (
+        <div className="flex items-center space-x-4">
+          <Image
+            src="/GPT-4.png"
+            width={150}
+            height={150}
+            alt="GPT-4"
+            className="rounded-full ml-4"
+          />
+          <div>
+            <h1 className="font-semibold text-2xl">GPT-4 API Access Checker</h1>
+            <p className="max-w-lg mt-2">
+              A handy and reliable tool that helps you check if you meet the
+              requirements for accessing the most powerful and versatile natural
+              language processing system ever created.
+            </p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="rounded-md outline-none p-2 text-black mt-4 w-full px-3"
+                placeholder="Enter your API Key here"
+                {...register("apiKey", { required: true })}
+                type="password"
+                required
+              />
+            </form>
+          </div>
         </div>
-      </div>
+      ) : page === "bad" ? (
+        <>
+          <FaXmark size={50} className="" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <h1 className="mt-8 text-2xl font-semibold">{details}</h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <button
+            onClick={() => returnHome()}
+            className="p-4 py-2 bg-zinc-800 mt-4 rounded-xl"
+          >
+            Return Home
+          </button>
+        </>
+      ) : page === "good" ? (
+        <>
+          <FaCheck size={50} className="" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <h1 className="mt-8 text-2xl font-semibold">
+            Your key has GPT-4 Access!
+          </h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <button
+            onClick={() => returnHome()}
+            className="p-4 py-2 bg-zinc-800 mt-4 rounded-xl"
+          >
+            Return Home
+          </button>
+        </>
+      ) : (
+        <>
+          <AiOutlineLoading size={50} className="animate-spin" />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <h1 className="mt-8 text-2xl font-semibold">Checking...</h1>
+        </>
+      )}
     </main>
-  )
+  );
 }
